bd1cf5db2f4497cad7d7f75cc71aa8320a35a144
jwjung <>
2018-03-12 22:58:53 +0900

    Commit!

--- MybatisTest/src/main/java/com/lsg/www/mapper/MainMapper.java
+++ MybatisTest/src/main/java/com/lsg/www/mapper/MainMapper.java
@@ -3,10 +3,7 @@ package com.lsg.www.mapper;
 import java.util.HashMap;
 
 import com.lsg.www.vo.YsCoptVo;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
-@Mapper
 public interface MainMapper {
 	public HashMap getYsCopt(YsCoptVo vo) throws Exception;
 }
--- MybatisTest/src/main/java/com/lsg/www/mapper/YsMemMapper.java
+++ MybatisTest/src/main/java/com/lsg/www/mapper/YsMemMapper.java
@@ -1,13 +1,9 @@
 package com.lsg.www.mapper;
 
 import com.lsg.www.vo.YsMemVO;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 
-@Mapper
-@Repository
 public interface YsMemMapper {
 	public HashMap getYsMem(YsMemVO vo) throws Exception;
 	public void insYsMem(YsMemVO vo) throws  Exception;
