8c1fd9cb62cc97bbcbac2cc7274d9b9ef4f5fa9d
jwjung <>
2018-03-12 22:27:35 +0900

    Commit!

--- MybatisTest/src/main/java/com/lsg/www/mapper/MainMapper.java
+++ MybatisTest/src/main/java/com/lsg/www/mapper/MainMapper.java
@@ -3,7 +3,11 @@ package com.lsg.www.mapper;
 import java.util.HashMap;
 
 import com.lsg.www.vo.YsCoptVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Mapper
+@Repository
 public interface MainMapper {
 	public HashMap getYsCopt(YsCoptVo vo) throws Exception;
 }
