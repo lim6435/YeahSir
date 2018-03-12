a6a1e06523bbc1c5d4460221d548891d3a313916
jwjung <>
2018-03-12 19:42:59 +0900

    Merge branch 'master' of https://github.com/lim6435/YeahSir
    # Conflicts:
    #	MybatisTest/src/main/java/com/lsg/www/controller/WebRestController.java

--- MybatisTest/src/main/java/com/lsg/www/controller/WebRestController.java
+++ MybatisTest/src/main/java/com/lsg/www/controller/WebRestController.java
@@ -1,10 +1,13 @@
 package com.lsg.www.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,19 +30,23 @@ public class WebRestController {
 	
 	@RequestMapping(value="/test", method= {RequestMethod.GET, RequestMethod.POST})
 	public String test(HttpServletRequest req, HttpServletResponse res) throws Exception{
-		List result = ysMemMapper.getYsMem("11");
-		
-		ObjectMapper objetMapper = new ObjectMapper();
-		ObjectWriter writer = objetMapper.writerWithDefaultPrettyPrinter();
-		
+		List result = ysMemMapper.getYsMem(req.getParameter("id"));
+
+		JSONArray jsonArray = new JSONArray();
+
+		for(int i = 0; i<result.size(); i++) {
+			JSONObject obj = new JSONObject();
+			obj.putAll((HashMap)result);
+			jsonArray.add(obj);
+		}
+
 		log.info("result :: ", result.toString());
 		System.out.println(result.toString());
 		
-		String json = writer.writeValueAsString(result);
-		log.info("result json :: " + json);
-		log.info(json);
-		System.out.println("result json :: " + json);
+		log.info("result json :: " + jsonArray.toString());
+		log.info(jsonArray.toString());
+		System.out.println("result json :: " + jsonArray.toString());
 		
-		return result.toString();
+		return jsonArray.toString();
 	}
 }
