625559fc5a7d566d0a64d380f5b264efac6286bb
jwjung <>
2018-03-13 20:27:00 +0900

    Commit! 처리!!!

--- MybatisTest/src/main/java/com/lsg/www/controller/WebRestController.java
+++ MybatisTest/src/main/java/com/lsg/www/controller/WebRestController.java
@@ -1,7 +1,10 @@
 package com.lsg.www.controller;
 
+import com.lsg.www.mapper.MainMapper;
 import com.lsg.www.mapper.YsMemMapper;
+import com.lsg.www.vo.YsCoptVo;
 import com.lsg.www.vo.YsMemVO;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class WebRestController {
@@ -16,6 +20,8 @@ public class WebRestController {
 
 	@Autowired
 	private YsMemMapper ysMemMapper;
+	@Autowired
+    private MainMapper mainMapper;
 	
 	@RequestMapping(value="/test", method= {RequestMethod.GET, RequestMethod.POST})
 	public String test(@RequestBody JSONObject object) throws Exception{
@@ -28,10 +34,16 @@ public class WebRestController {
 
 	    log.info("Request Parameter : " + reqId + " \t##### pwd : " + password);
 		HashMap result = ysMemMapper.getYsMem(memVo);
+        YsCoptVo coptVo = new YsCoptVo();
+        List coptList = mainMapper.getYsCopt(coptVo);
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(coptList);
 
         JSONObject obj = new JSONObject();
         obj.putAll(result);
-		log.info("result :: " + result.toString());
+        obj.put("getCoptInfo", jsonArray.toJSONString());
+//		log.info("result :: " + result.toString());
 		log.info("result json :: " + obj.toString());
 
 		return obj.toString();
