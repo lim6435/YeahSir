9130245beaeb30d2b40d4c639c604008048a4da1
jwjung <>
2018-03-13 22:56:28 +0900

    JSON 처리

--- MybatisTest/src/main/java/com/lsg/www/controller/JoinController.java
+++ MybatisTest/src/main/java/com/lsg/www/controller/JoinController.java
@@ -3,6 +3,7 @@ package com.lsg.www.controller;
 import com.google.gson.*;
 import com.lsg.www.vo.YsMemVO;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +23,24 @@ public class JoinController {
 	@RequestMapping(value="/memJoin", method= {RequestMethod.GET, RequestMethod.POST})
 	public String memJoin(@RequestBody String str) throws Exception{
 		log.info("Request Message : " + str);
-		JsonParser jsonParser = new JsonParser();
-
-		JsonObject object = (JsonObject) jsonParser.parse(str);
-		String memId = new String(object.get("id").getAsString().getBytes("UTF-8"), "UTF-8");
-		String memPwd = new String(object.get("pwd").getAsString().getBytes("UTF-8"), "UTF-8");
-		String memName = new String(object.get("memName").getAsString().getBytes("UTF-8"), "UTF-8");
-		String lgnTpcd = new String(object.get("lgnTpcd").getAsString().getBytes("UTF-8"), "UTF-8");
-		String memBirth = new String(object.get("memBirth").getAsString().getBytes("UTF-8"), "UTF-8");
-		String memTall = new String(object.get("memTall").getAsString().getBytes("UTF-8"), "UTF-8");
-		String memWeight = new String(object.get("memWeight").getAsString().getBytes("UTF-8"), "UTF-8");
+		JSONParser parser = new JSONParser();
+		str = new String(str.getBytes(), "UTF-8");
+		JSONObject object = (JSONObject) parser.parse(str);
+//		JsonObject object = (JsonObject) jsonParser.parse(str);
+//		String memId = new String(object.get("id").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String memPwd = new String(object.get("pwd").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String memName = new String(object.get("memName").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String lgnTpcd = new String(object.get("lgnTpcd").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String memBirth = new String(object.get("memBirth").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String memTall = new String(object.get("memTall").getAsString().getBytes("UTF-8"), "UTF-8");
+//		String memWeight = new String(object.get("memWeight").getAsString().getBytes("UTF-8"), "UTF-8");
+		String memId = (String)object.get("id");
+		String memPwd = (String)object.get("pwd");
+		String memName = (String)object.get("memName");
+		String lgnTpcd = (String)object.get("lgnTpcd");
+		String memBirth = (String)object.get("memBirth");
+		String memTall = (String)object.get("memTall");
+		String memWeight = (String)object.get("memWeight");
 		String regId = this.getClass().getSimpleName();
 
 		log.info("memId ::: " + memId);
